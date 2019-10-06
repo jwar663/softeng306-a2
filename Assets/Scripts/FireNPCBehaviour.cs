8754e6da9ed5fe8846dac6a829aa7910241106ae
Shunji Takano <stak469@aucklanduni.ac.nz>
2019-10-07 00:03:57 +1300

    added Dialogue functionality

--- Assets/Scripts/FireNPCBehaviour.cs
+++ Assets/Scripts/FireNPCBehaviour.cs
@@ -3,25 +3,33 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class FireNPCBehaviour : NPCBehaviour
-{   
+{
     // Start is called before the first frame update
     void Start()
     {
-        
+        FindObjectOfType<DialogueTrigger>().TriggerDialogue(sentences);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
-    
-    public override void interact(Player player) {
-        if (player.fireTreesLeft == 0) {
-            Debug.Log("you shall pass");
+
+    public override void interact(Player player)
+    {
+        if (player.fireTreesLeft == 0)
+        {
+            List<string> sentences = new List<string>();
+            sentences.Add("You shall pass");
+            FindObjectOfType<DialogueTrigger>().TriggerDialogue(sentences);
             Destroy(this.gameObject);
-        } else {
-            Debug.Log("you shall not pass");
+        }
+        else
+        {
+            List<string> sentences = new List<string>();
+            sentences.Add("You shall not pass");
+            FindObjectOfType<DialogueTrigger>().TriggerDialogue(sentences);
         }
     }
 }
