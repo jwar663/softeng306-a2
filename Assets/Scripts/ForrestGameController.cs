16f97c7aedf9ea6aa973e68ca65774a32e5a3861
amah460 <amah460@aucklanduni.ac.nz>
2019-10-07 22:26:35 +1300

    Majority of dialogue completed for forrest scene

--- Assets/Scripts/ForrestGameController.cs
+++ Assets/Scripts/ForrestGameController.cs
@@ -43,15 +43,13 @@ public class ForrestGameController : NPCBehaviour
     IEnumerator Run()
     {
         camera.GetComponent<CameraMovement>().enabled = false;
-        yield return new WaitForSeconds(1f);
+        yield return new WaitForSeconds(0.5f);
         List<string> dialogue = new List<string>();
-        dialogue.Add("Look at the world.....");
-        dialogue.Add("....Look at what climate change has done.");
-        dialogue.Add("...it has destroyed the world irreversably.");
-        dialogue.Add("But there is still hope.");
-        dialogue.Add("You must travel back in time to climate disasters and stop them from ever happening");
-        dialogue.Add("You are humanity's last hope!");
-        dialogue.Add("Good luck!");
+        dialogue.Add("Welcome to the rainforest.");
+        dialogue.Add("Home to thousands of animal and plant species.");
+        dialogue.Add("Rainforest play a huge role in regulating the worlds CO2 levels.");
+        dialogue.Add("Each year we humans set them on fire to make way for agriculatural land.");
+        dialogue.Add("If we dont put a stop to this, we wont have any rainforests left.");
         FindObjectOfType<DialogueTrigger>().TriggerDialogue(npc, dialogue);
         isCompleted = false;
         yield return null;
@@ -65,15 +63,8 @@ public class ForrestGameController : NPCBehaviour
             moveCamera(camera.transform.position, portal.position, camera);
             yield return null;
         }
-
         dialogue = new List<string>();
-        dialogue.Add("Look at the world.....");
-        dialogue.Add("....Look at what climate change has done.");
-        dialogue.Add("...it has destroyed the world irreversably.");
-        dialogue.Add("But there is still hope.");
-        dialogue.Add("You must travel back in time to climate disasters and stop them from ever happening");
-        dialogue.Add("You are humanity's last hope!");
-        dialogue.Add("Good luck!");
+        dialogue.Add("You can use that portal to leave the area.");
         FindObjectOfType<DialogueTrigger>().TriggerDialogue(npc, dialogue);
         isCompleted = false;
         yield return null;
@@ -81,6 +72,53 @@ public class ForrestGameController : NPCBehaviour
         {
             yield return null;
         }
+
+        for (int i = 0; i < 200; i++)
+        {
+            moveCamera(camera.transform.position, largeFire.position, camera);
+            yield return null;
+        }
+        dialogue = new List<string>();
+        dialogue.Add("The portal is being guarded by that fire monster.");
+        FindObjectOfType<DialogueTrigger>().TriggerDialogue(npc, dialogue);
+        isCompleted = false;
+        yield return null;
+        while (!isCompleted)
+        {
+            yield return null;
+        }
+
+        for (int i = 0; i < 200; i++)
+        {
+            moveCamera(camera.transform.position, burningTree.position, camera);
+            yield return null;
+        }
+        dialogue = new List<string>();
+        dialogue.Add("You must put out the burning trees to weaken the fire monster.");
+        dialogue.Add("If you put out enough fires, he will be too weak to continue burning and go out.");
+        FindObjectOfType<DialogueTrigger>().TriggerDialogue(npc, dialogue);
+        isCompleted = false;
+        yield return null;
+        while (!isCompleted)
+        {
+            yield return null;
+        }
+
+        for (int i = 0; i < 200; i++)
+        {
+            moveCamera(camera.transform.position, smallFire.position, camera);
+            yield return null;
+        }
+        dialogue = new List<string>();
+        dialogue.Add("Try and avoid these smaller fire monsters, they will hurt you.");
+        FindObjectOfType<DialogueTrigger>().TriggerDialogue(npc, dialogue);
+        isCompleted = false;
+        yield return null;
+        while (!isCompleted)
+        {
+            yield return null;
+        }
+
         camera.GetComponent<CameraMovement>().enabled = true;
     }
 }
