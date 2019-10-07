10a217d07e385e29d6a12517b7da83a5ac7e356f
veratrum <veratrum@users.noreply.github.com>
2019-10-07 13:34:36 +1300

    Added transition scene at the start of a level

---
+++ Assets/Scripts/LevelStartTransition.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class LevelStartTransition : MonoBehaviour
+{
+    private Text titleText;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        titleText = GetComponent<Text>();
+        
+        if (Game.levelID == -1) {
+            titleText.text = Game.levelName;
+        } else {
+            titleText.text = "Level " + Game.levelID + ": " + Game.levelName;
+        }
+        
+        Invoke("startScene", 2.0f);
+    }
+    
+    void startScene() {
+        SceneManager.LoadScene(Game.sceneName);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
