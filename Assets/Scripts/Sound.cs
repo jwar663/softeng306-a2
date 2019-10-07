738d5bbd7014e189724aebed34128eab19a25f70
jwar663 <jwar663@users.noreply.github.com>
2019-10-07 15:14:44 +1300

    Audio Implementation
    Added an audio manager, and added a theme song to forest scene

---
+++ Assets/Scripts/Sound.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine.Audio;
+using UnityEngine;
+
+[System.Serializable]
+public class Sound
+{
+    //name of clip
+    public string name;
+
+    //actual clip object
+    public AudioClip clip;
+
+    //slider for volume
+    [Range(0f, 1f)]
+    public float volume;
+
+    //slider for pitch
+    [Range(.1f, 3f)]
+    public float pitch;
+
+    //whether sound should loop or not
+    public bool loop;
+
+    //whether the sound should be represented at 2d or 3d
+    [Range(0f, 1f)]
+    public float spatialBlend;
+
+    [HideInInspector]
+    public AudioSource source;
+}
