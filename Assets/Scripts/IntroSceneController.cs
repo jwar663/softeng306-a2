5c05141d1a3f39fd3c91632a4be16477744eeb93
amah460 <amah460@aucklanduni.ac.nz>
2019-10-07 16:27:30 +1300

    Addition of assets relating to intro Scene
    Added a Scene to show player what the story is about, the scene is still incomplete

---
+++ Assets/Scripts/IntroSceneController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IntroSceneController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        List<string> dialogue = new List<string>();
+        dialogue.Add("Hello worlds");
+        FindObjectOfType<DialogueTrigger>().TriggerDialogue(dialogue);   
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
