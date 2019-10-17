046984ef2f019c016b7640400ab17b78f5ee1083
veratrum <veratrum@users.noreply.github.com>
2019-10-18 12:54:22 +1300

    Fixed issues with NPC

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -110,7 +110,7 @@ public class Player : MonoBehaviour
         }
         
         // if 'x' pressed
-        if (Input.GetKey(KeyCode.X) || Input.GetKey(KeyCode.Space)) {
+        if (Input.GetKeyDown(KeyCode.X) || Input.GetKeyDown(KeyCode.Space)) {
             // calculate what square the player is facing
             Vector2 offset;
             switch (facingDirection) {
