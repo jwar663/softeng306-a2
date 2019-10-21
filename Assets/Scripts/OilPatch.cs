75e4224d1790a827ccc646efe6dc017108a2a3af
amah460 <amah460@aucklanduni.ac.nz>
2019-10-21 22:52:46 +1300

    Addition of Oil level which is mostly working apart from scoring and level completion

---
+++ Assets/Scripts/OilPatch.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OilPatch : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void SuckOil()
+    {
+        StartCoroutine("Run");
+    }
+
+    IEnumerator Run()
+    {
+        for(int i = 20; i>0; i--)
+        {
+            transform.localScale = new Vector3(0.050f*i, 0.050f * i, 0.050f * i);
+            yield return new WaitForSeconds(0.05f);
+        }
+        Destroy(gameObject);
+    }
+}
