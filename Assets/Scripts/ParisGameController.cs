684a458fc09ab0f1e351bf788347cab9a6599949
veratrum <veratrum@users.noreply.github.com>
2019-10-21 16:23:49 +1300

    Added UI and UI accessories to Paris scene. hope this merges

---
+++ Assets/Scripts/ParisGameController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParisGameController: MonoBehaviour, LevelController {
+    private List<NPC> npcs;
+    
+    void Start() {
+        npcs = new List<NPC>(FindObjectsOfType<NPC>());
+    }
+
+    void Update() {
+        
+    }
+    
+    public List<NPC> getNPCs() {
+        return npcs;
+    }
+}
