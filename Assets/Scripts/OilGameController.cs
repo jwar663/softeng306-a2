2594f49ba7273cefabe2b956d1c879b66aaf103b
Shunji Takano <stak469@aucklanduni.ac.nz>
2019-10-22 01:59:15 +1300

    Added portal to oil scene

---
+++ Assets/Scripts/OilGameController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class OilGameController : MonoBehaviour
+{
+
+    public Camera camera;
+    private Player player;
+    public int score;
+    public Text scoreText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = FindObjectOfType<Player>();
+        this.score = 10;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        scoreText.text = "Oil spills left: " + score.ToString();
+        GameManager.getInstance().score = 10 - score;
+        GameManager.getInstance().time = Time.timeSinceLevelLoad;
+    }
+}
